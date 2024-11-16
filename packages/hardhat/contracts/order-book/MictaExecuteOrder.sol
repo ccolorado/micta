8700c22b3c87351f440d33fb0326330e9b69fb56
cNoveron <carlos.escom.ipn@gmail.com>
2024-11-16 22:30:39 +0700

    dev: cleanup formatting

--- packages/hardhat/contracts/order-book/MictaExecuteOrder.sol
+++ packages/hardhat/contracts/order-book/MictaExecuteOrder.sol
@@ -9,6 +9,7 @@ contract MictaExecuteOrder {
     address mailboxHyperlane;
     address lastSender;
     bytes lastData;
+
     event ReceivedMessage(uint origin, address sender, uint value, string data);
 
     constructor(
@@ -18,30 +19,30 @@ contract MictaExecuteOrder {
     }
 
     function handle(
-    uint32 _origin,
-    bytes32 _sender,
-    bytes calldata _data
-) external payable virtual {
-    // check sender
-    if (msg.sender != mailboxHyperlane) {
-            revert();
-        }
-    if (
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata _data
+    ) external payable virtual {
+        // check sender
+        if(msg.sender != mailboxHyperlane) revert();
+
+        if(
             address(uint160(uint256(_sender))) != mailboxHyperlane &&
             _origin != 421614
         ) {
             revert();
         }
-    (address user, bytes memory hash_digest) = abi.decode(
+
+        (address user, bytes memory hash_digest) = abi.decode(
             _data,
             (address, bytes)
         );
-    lastSender = user;
-    lastData = hash_digest;
-}
+        lastSender = user;
+        lastData = hash_digest;
+    }
 
-function executeOrder() public {
-// TODO: Implement the COW Swap contracts
-    
-}
+    function executeOrder() public {
+      // TODO: Implement the COW Swap contracts
+
+    }
 }
